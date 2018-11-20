6afc47d90d7349c78db8a88419309259af3137cc
TIMEZR <2545495010@qq.com>
2018-11-20 20:29:34 +0800

    2018-11-20

---
+++ src/pages/home/HomeContainer.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import { TabBar } from 'antd-mobile' 
+
+export class HomeContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedTab: 'redTab',
+          hidden: false,
+          fullScreen: true,
+        }
+    }
+    render(){
+        return(
+            <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="Life"
+            key="Life"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(//xs01.meituan.net/waimai_i/img/sprite/tab-icons.949e08ab.png) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(//xs01.meituan.net/waimai_i/img/sprite/tab-icons.949e08ab.png) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selected={this.state.selectedTab === 'blueTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            {/* {this.renderContent('Life')} */}
+            <div>1</div>
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="Koubei"
+            key="Koubei"
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+            {/* {this.renderContent('Koubei')} */}
+            <div>2</div>
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="My"
+            key="my"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+            {/* {this.renderContent('My')} */}
+            <div>3</div>
+          </TabBar.Item>
+        </TabBar>
+      </div>
+        )
+    }
+}
+
+export default HomeContainer
